9ecd020efa2dbda9e4ded3ef2c901c924f233a3c
Jhon Mario Lotero Salazar <jhoning.soft@gmail.com>
2021-06-15 10:21:51 -0500

    Add fonts typegraphics
    Add fonts typegraphics

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -54,6 +54,7 @@ class DescriptionPlace extends StatelessWidget {
       child: new Text(
         this.descriptionPlace,
         style: const TextStyle(
+          fontFamily: "Lato",
           fontSize: 16.0,
           fontWeight: FontWeight.bold,
           color: Color(0xFF56575a)
@@ -72,6 +73,7 @@ class DescriptionPlace extends StatelessWidget {
             child: Text(
               this.namePlace,
               style : TextStyle(
+                fontFamily: "Lato",
                 fontSize: 30.0,
                 fontWeight: FontWeight.w900
               ),
