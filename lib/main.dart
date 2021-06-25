40669aae271700429dac4509442fd09b5e57f3b3
Jhon Mario Lotero Salazar <jhoning.soft@gmail.com>
2021-06-24 19:53:02 -0500

    BloC provider and firebase dependencies
    * Start BloC provider pattern implementation
    * Firebase dependencies and android app registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:trips_app_flutter/navigation_bar_trips_cupertino.dart';
+import 'package:trips_app_flutter/user/bloc/bloc_user.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]); // remove the android status bar color
-    return MaterialApp(
-      title: 'Flutter Demo', // title of the task manager
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      child: MaterialApp(
+          title: 'Flutter Demo', // title of the task manager
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: NavigationBarTripsCupertino()
       ),
-      home: NavigationBarTripsCupertino()
+      bloc: UserBloc(), // All methos of userBloc can be accessed by MaterialApp
     );
   }
 }
